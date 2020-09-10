5e2e849373f215f52956a5d89078fe30d9961d48
James Milne <s4b3r6@gmail.com>
2020-09-10 13:25:16 +1000

    Show progress for series, where appropriate

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+function seconds_to_stamp(duration)
+{   
+    // Hours, minutes and seconds
+    var hrs = ~~(duration / 3600);
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
 function titleCase(str) {
 	str = str.split(' ');
 	for (var i = 0; i < str.length; i++) {
@@ -136,6 +155,15 @@ function load_series(uuid) {
 					var tmp_desc = document.createElement('small')
 					tmp_desc.textContent = data.children[i].description;
 					tmp.appendChild(tmp_desc);
+
+					if(!!data.children[i].progress) {
+						var tmp_time = document.createElement('small');
+						tmp_time.textContent = "<progress> / <runtime>"
+						.replace("<progress>", seconds_to_stamp(data.children[i].progress))
+						.replace("<runtime>", seconds_to_stamp(data.children[i].runtime));
+
+						tmp.appendChild(tmp_time);
+					}
 
 					collection.appendChild(tmp);
   				}
